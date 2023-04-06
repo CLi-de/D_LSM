8f74a417c5cf0b4592b65cb811d7233ea2c2c55f
CLi-de <browsercl@gmail.com>
2023-04-06 14:27:03 +0800

    meta-learning modeling

--- util.py
+++ util.py
@@ -8,6 +8,9 @@
 import numpy as np
 import pandas as pd
 
+import tensorflow as tf
+import tf_slim as slim
+
 
 def cal_measure(pred, y_test):
     TP = ((pred == 1) * (y_test == 1)).astype(int).sum()
@@ -30,3 +33,26 @@ def pred_LSM(trained_model, xy, samples, name):
     data_df.to_excel(writer, 'page_1', float_format='%.5f')
     writer.close()
 
+
+def normalize(inp, activation, reuse, scope, norm):
+    if norm == 'batch_norm':
+        return slim.batch_norm(inp, activation_fn=activation, reuse=reuse, scope=scope)
+    elif norm == 'layer_norm':
+        return slim.layer_norm(inp, activation_fn=activation, reuse=reuse, scope=scope)
+    elif norm == 'None':
+        if activation is not None:
+            return activation(inp)
+        else:
+            return inp
+
+
+def mse(pred, label):
+    pred = tf.reshape(pred, [-1])
+    label = tf.reshape(label, [-1])
+    return tf.reduce_mean(input_tensor=tf.square(pred - label))
+
+
+def xent(pred, label):
+    # Note - with tf version <=0.12, this loss has incorrect 2nd derivatives
+    return tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=label) / tf.cast(tf.shape(input=label)[0],
+                                                                                        dtype=tf.float32)  # 注意归一
